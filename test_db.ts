717a6ef291987db0bbd7603b65bf9955bc7da302
Ali Akpoyraz <aliakpoyraz@gmail.com>
2026-03-02 12:20:21 +0300

    Delete test_db.ts

--- test_db.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const v = await prisma.variant.findFirst();
-  console.log(v);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
