b24f9ab62b638836c251c940849ae34842a59d90
Ali Akpoyraz <128495051+aliakpoyraz@users.noreply.github.com>
2026-03-06 21:06:34 +0300

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+export const dynamic = 'force-dynamic';
 const inter = Inter({
   subsets: ["latin"],
   display: 'swap',
